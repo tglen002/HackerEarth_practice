bb326205136118b6fdf759ab25f39d7b83d6edb9
Tiffany Glenn-Hall <tglen002@ucr.edu>
2014-09-06 00:32:30 -0700

    HackerEarth Practice Problems
    Different programs to practice writing programs.

---
+++ Test1.cpp
@@ -0,0 +1,18 @@
+//
+//  Test1.cpp
+//  Wendy_and_Robos
+//
+//  Created by Tiffany Glenn-Hall on 9/6/14.
+//
+//
+
+#include "Test1.h"
+#include <string>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+int main(){
+    
+}
